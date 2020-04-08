8c9d976c69d6cd521c994c7ef2028776f7514318
Jesko Hecking-Harbusch <13851486+jeskoheha@users.noreply.github.com>
2020-04-08 15:33:48 +0200

    JHH: fix another naming problem in AdamModelChecker and add test that uses existing generator to build small office to test FlowCTL in the future

---
+++ test/uniolunisaar/adam/modelchecker/ctl/TestingModelcheckingAccessControl.java
@@ -0,0 +1,125 @@
+package uniolunisaar.adam.modelchecker.ctl;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.testng.Assert;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import uniol.apt.adt.pn.Place;
+import uniol.apt.adt.pn.Transition;
+import uniol.apt.io.parser.ParseException;
+import uniol.apt.io.renderer.RenderException;
+import uniol.apt.util.Pair;
+import uniolunisaar.adam.ds.logics.ctl.flowctl.RunCTLFormula;
+import uniolunisaar.adam.ds.modelchecking.results.CTLModelcheckingResult;
+import uniolunisaar.adam.ds.modelchecking.results.ModelCheckingResult;
+import uniolunisaar.adam.ds.modelchecking.settings.ctl.CTLLoLAModelcheckingSettings;
+import uniolunisaar.adam.ds.petrinet.PetriNetExtensionHandler;
+import uniolunisaar.adam.ds.petrinetwithtransits.PetriNetWithTransits;
+import uniolunisaar.adam.util.PNWTTools;
+import uniolunisaar.adam.exceptions.ExternalToolException;
+import uniolunisaar.adam.exceptions.ProcessNotStartedException;
+import uniolunisaar.adam.generators.pnwt.util.acencoding.AccessControl;
+import uniolunisaar.adam.logic.modelchecking.ctl.ModelCheckerCTL;
+import uniolunisaar.adam.logic.parser.logics.flowctl.FlowCTLParser;
+import uniolunisaar.adam.tools.Logger;
+
+/**
+ *
+ * @author Manuel Gieseking
+ */
+@Test
+public class TestingModelcheckingAccessControl {
+
+    private static final String outputDir = System.getProperty("testoutputfolder") + "/ctl/";
+
+    @BeforeClass
+    public void createFolder() {
+        (new File(outputDir)).mkdirs();
+    }
+
+    @BeforeClass
+    public void silence() {
+        Logger.getInstance().setVerbose(true);
+//        Logger.getInstance().setVerbose(false);
+//        Logger.getInstance().setShortMessageStream(null);
+//        Logger.getInstance().setVerboseMessageStream(null);
+//        Logger.getInstance().setWarningStream(null);
+    }
+
+    @BeforeClass
+    public void setProperties() {
+        if (System.getProperty("examplesfolder") == null) {
+            System.setProperty("examplesfolder", "examples");
+        }
+    }
+
+    @Test
+    void testAccessControl() throws Exception {
+    	String name = "officeSmall";
+		Set<String> groups = new HashSet<>();
+		groups.add("it");
+		
+		Set<String> locations = new HashSet<>();
+		locations.add("out");
+		locations.add("lob");
+		locations.add("cor");
+		locations.add("mr");
+		locations.add("bur");
+		
+		Map<String, String> starts = new HashMap<>();
+		starts.put("it", "out");
+		
+		Set<Pair<String, String>> connections = new HashSet<>();
+		connections.add(new Pair<String, String>("out", "lob"));
+		connections.add(new Pair<String, String>("lob", "out"));
+		
+		connections.add(new Pair<String, String>("out", "cor"));
+		connections.add(new Pair<String, String>("cor", "out"));
+		
+		connections.add(new Pair<String, String>("lob", "cor"));
+		connections.add(new Pair<String, String>("cor", "lob"));
+
+		connections.add(new Pair<String, String>("cor", "mr"));
+		connections.add(new Pair<String, String>("mr", "cor"));
+		
+		connections.add(new Pair<String, String>("cor", "bur"));
+		connections.add(new Pair<String, String>("bur", "cor"));
+		
+		Map<String, Set<Pair<String, String>>> open = new HashMap<>();
+		
+		Set<Pair<String, String>> itOpen = new HashSet<>();
+		itOpen.add(new Pair<String, String>("out", "lob"));
+		itOpen.add(new Pair<String, String>("lob", "out"));
+		itOpen.add(new Pair<String, String>("out", "cor"));
+		itOpen.add(new Pair<String, String>("cor", "out"));
+		itOpen.add(new Pair<String, String>("lob", "cor"));
+		itOpen.add(new Pair<String, String>("cor", "lob"));
+		itOpen.add(new Pair<String, String>("cor", "mr"));
+		itOpen.add(new Pair<String, String>("mr", "cor"));
+		itOpen.add(new Pair<String, String>("cor", "bur"));
+		itOpen.add(new Pair<String, String>("bur", "cor"));
+		open.put("it", itOpen);
+		
+		PetriNetWithTransits net = new AccessControl(name, groups, locations, starts, connections, open).createAccessControlExample();
+        
+		PNWTTools.savePnwt2PDF(outputDir + net.getName(), net, true);
+
+        CTLLoLAModelcheckingSettings settings = new CTLLoLAModelcheckingSettings(outputDir + net.getName());
+        ModelCheckerCTL mc = new ModelCheckerCTL(settings);
+        RunCTLFormula formula = FlowCTLParser.parse(net, "AF TRUE");
+        CTLModelcheckingResult result = mc.check(net, formula.toCTLFormula());
+        Logger.getInstance().addMessage("ERROR:");
+        Logger.getInstance().addMessage(result.getLolaError());
+        Logger.getInstance().addMessage("OUTPUT:");
+        Logger.getInstance().addMessage(result.getLolaOutput());
+//        Assert.assertEquals(result.getSatisfied() , ModelCheckingResult.Satisfied.TRUE);
+	}
+}
